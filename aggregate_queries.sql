1d95b11a8c3c6109bc85edfe99c205687b7a22cb
Paddy Mullen <paddy@paddymullen.com>
2014-06-28 10:52:41 -0400

    first average time differences

--- aggregate_queries.sql
+++ aggregate_queries.sql
@@ -1,9 +1,18 @@
 SELECT
-        start_station_id, end_station_id, count(*) AS cnt 
+        stt.start_station_id, stt.end_station_id, count(*) AS cnt,
+        avg(taxi_trips.trip_time_in_secs) AS avg_taxi_time,
+        avg(citibike_trips.trip_duration) AS avg_cb_time,
+        avg(citibike_trips.trip_duration) - avg(taxi_trips.trip_time_in_secs) AS time_diff
 FROM
-        similar_taxi_trips 
+        similar_taxi_trips AS stt,
+        taxi_trips,
+        citibike_trips
+WHERE
+        stt.taxi_trip_id = taxi_trips.taxi_trip_id AND
+        stt.start_station_id  = citibike_trips.start_station_id AND
+        stt.end_station_id  = citibike_trips.end_station_id 
 GROUP BY
-        start_station_id, end_station_id 
+        stt.start_station_id, stt.end_station_id 
 ORDER BY
         cnt DESC 
 LIMIT 30;
